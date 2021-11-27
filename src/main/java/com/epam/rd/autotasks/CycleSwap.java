9f51e0c46dd0e9a09311ca526435a790e78e00cb
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-27 21:41:28 +0200

    completed task

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,31 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
+        if (array.length > 1) {
+
+            int lastIndex = array.length - 1;
+            int oldLast = array[lastIndex];
+            for (int i = lastIndex; i != 0; i--)
+                array[i] = array[i - 1];
+
+            array[0] = oldLast;
+        } else array = null;
+
 
     }
 
     static void cycleSwap(int[] array, int shift) {
+        if (array.length > 1) {
+            for (int j = 0; j < shift; j++) {
+                int lastIndex = array.length - 1;
+                int oldLast = array[lastIndex];
+                for (int i = lastIndex; i != 0; i--)
+                    array[i] = array[i - 1];
+
+                array[0] = oldLast;
+
+            }
+        }
 
     }
 }
